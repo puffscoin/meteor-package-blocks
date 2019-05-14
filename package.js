73ba8b96e37f7983081cddc24f5ece91aa8ba5b9
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-05-14 15:56:22 -0300

    Update package.js

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
-  name: "ethereum:blocks",
-  summary: "Provides informations about the current and last 50 blocks",
+  name: "puffscoin:blocks",
+  summary: "Provides informations about the current and last 50 puffscoin blocks",
   version: "1.1.0",
-  git: "http://github.com/ethereum/meteor-package-blocks"
+  git: "http://github.com/puffscoin/meteor-package-blocks"
 });
 
 Package.onUse(function(api) {
@@ -11,9 +11,9 @@ Package.onUse(function(api) {
   api.use("mongo", ["client", "server"]);
 
   // api.use('frozeman:persistent-minimongo@0.1.3', 'client');
-  api.use("ethereum:web3@1.0.0-beta.33", ["client", "server"]);
+  api.use("puffscoin:web3@1.0.0-beta.33", ["client", "server"]);
 
-  api.export(["EthBlocks"], ["client", "server"]);
+  api.export(["PuffsBlocks"], ["client", "server"]);
 
   api.addFiles("blocks.js", ["client", "server"]);
 });
